009ffa5f01eaefeb6139e6d1c233505b560e83f0
LotosArt <lotos811@gmail.com>
2020-08-07 11:17:11 +0300

    Live search native js

---
+++ src/js/main.js
@@ -0,0 +1,53 @@
+let $input = document.querySelector('#query');
+let $list = document.querySelector('.drop-list');
+let $form = document.querySelector('.form');
+
+let charcters = [];
+
+function updateList(list) {
+    let template = '';
+
+    if (list.length < 1) {
+        template = 'Not found';
+    } else {
+        for (let i = 0; i < list.length; i++) {
+            template += '<li>' + list[i].name + '</li>';
+        }
+    }
+
+    $list.innerHTML = template;
+}
+
+fetch('https://pokeapi.co/api/v2/pokemon/')
+    .then(function (responce) {
+        return responce.json();
+    })
+    .then(function (data) {
+        charcters = data.results;
+        updateList(charcters);
+        console.log(charcters);
+    });
+
+$input.addEventListener('input', function () {
+    let query = $input.value.toLowerCase();
+    let buffer = charcters;
+
+    buffer = buffer.filter(function (character) {
+        return ~character.name.toLowerCase().indexOf(query);
+    });
+
+    updateList(buffer);
+});
+
+$input.addEventListener('focus', function () {
+    $form.classList.add('active');
+});
+
+$input.addEventListener('blur', function () {
+    $form.classList.remove('active');
+});
+
+$list.addEventListener('click', function (event) {
+    console.log(event.target.innerText);
+    $input.value = event.target.innerText;
+});
